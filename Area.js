0da6f2c9dc2f58e892761b711defeff8844584dd
Drkphl <leonardotizon@hotmail.com>
2024-07-08 18:18:00 -0300

    primeiro commit

---
+++ Area.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')({ sigint: true });
+const Func = require('CalculoArea/Funcoes.js');
+
+let forma = prompt('qual forma geometrica iremos calcular a área?');
+
+switch (forma) {
+    case 'Circulo':
+        break;
+    case 'Retangulo':
+        break;
+    case 'Retangulo':
+        break;
+    default:
+        break;
+}
---
+++ Funcoes.js
@@ -0,0 +1,14 @@
+function TriangArea(num1, num2) {
+    area = (num1 * num2) / 2;
+}
+module.exports = TriangArea;
+
+function CircArea(num1) {
+    area = 2 * Math.PI * (num1 * num1);
+}
+module.exports = CircArea;
+
+function RetangArea(num1, num2) {
+    area = num1 * num2;
+}
+module.exports = RetangArea;
